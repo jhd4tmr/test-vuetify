e8b6dc9a07cc51a4ec6ae5d2db5132256ec92adf
jhd <jhd2215583@qq.com>
2019-09-04 11:04:13 +0800

    first commit

--- pages/table.vue
+++ pages/table.vue
@@ -1,5 +1,22 @@
 <template>
-  $END$
+  <v-simple-table>
+    <thead>
+      <tr>
+        <th>name</th>
+        <th>age</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Joe</td>
+        <td>18</td>
+      </tr>
+      <tr>
+        <td>Lily</td>
+        <td>20</td>
+      </tr>
+    </tbody>
+  </v-simple-table>
 </template>
 
 <script>
